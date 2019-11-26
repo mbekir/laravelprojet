7626b4641f0b81a2be82a449e7ef361458362db2
foued603 <fouda603@gmail.com>
2019-11-26 12:11:51 +0100

    Mise a jour foued lameri

---
+++ database/migrations/2019_11_26_110649_create_joueurs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJoueursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('joueurs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('datedenaissance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('joueurs');
+    }
+}
